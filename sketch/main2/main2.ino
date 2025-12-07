8c04a2c8cadc22474b26ab37d76c8b22be9e2acd
thesun_Rider <suryasaradhi3@gmail.com>
2025-12-07 20:18:01 +0100

    Added base codes

--- sketch/main2/main2.ino
+++ sketch/main2/main2.ino
@@ -15,17 +15,15 @@
  ***************************************************************************************/
 
 #include <ESP32-HUB75-MatrixPanel-I2S-DMA.h>
-#include <WiFi.h>
-#include <ESPmDNS.h>
-#include <NetworkUdp.h>
-#include <ArduinoOTA.h>
+  #include <WiFi.h>
+  #include <WiFiClient.h>
+  #include <WebServer.h>
 
+#include <ElegantOTA.h>
 
-
-
+WebServer server(80);
 const char *ssid = "unisiegen";
 const char *password = "eLab4Zimtsuper#sicher";
-uint32_t last_ota_time = 0;
 
 //------------------------------------------------------------------------------------------------------------------
 
@@ -108,6 +106,7 @@ void setup() {
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
+    // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -117,44 +116,14 @@ void setup() {
   Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-
-  ArduinoOTA
-    .onStart([]() {
-      String type;
-      if (ArduinoOTA.getCommand() == U_FLASH) {
-        type = "sketch";
-      } else {  // U_SPIFFS
-        type = "filesystem";
-      }
-
-      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-      Serial.println("Start updating " + type);
-    })
-    .onEnd([]() {
-      Serial.println("\nEnd");
-    })
-    .onProgress([](unsigned int progress, unsigned int total) {
-      if (millis() - last_ota_time > 500) {
-        Serial.printf("Progress: %u%%\n", (progress / (total / 100)));
-        last_ota_time = millis();
-      }
-    })
-    .onError([](ota_error_t error) {
-      Serial.printf("Error[%u]: ", error);
-      if (error == OTA_AUTH_ERROR) {
-        Serial.println("Auth Failed");
-      } else if (error == OTA_BEGIN_ERROR) {
-        Serial.println("Begin Failed");
-      } else if (error == OTA_CONNECT_ERROR) {
-        Serial.println("Connect Failed");
-      } else if (error == OTA_RECEIVE_ERROR) {
-        Serial.println("Receive Failed");
-      } else if (error == OTA_END_ERROR) {
-        Serial.println("End Failed");
-      }
-    });
-
-  ArduinoOTA.begin();
+ 
+  server.on("/", []() {
+    server.send(200, "text/plain", "Hi! This is ElegantOTA Demo.");
+  });
+ 
+  ElegantOTA.begin(&server);    // Start ElegantOTA
+  server.begin();
+  Serial.println("HTTP server started");
 
   Serial.println("Ready");
   Serial.print("IP address: ");
@@ -209,7 +178,8 @@ String text = "ELAB IS OPEN WELCOME!";
 
 void loop() {
 
-  ArduinoOTA.handle();
+  server.handleClient();
+  ElegantOTA.loop();
 
 //  http://141.99.58.241/update
 
